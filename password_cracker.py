54e62e76e7538036ec5bd3d5ed771926028990c8
BRIAN AKASHABA <akashaba@gmail.com>
2025-02-25 18:42:45 +0000

    password craker

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,22 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts = False):
-    return True
+  pwd = open('top-10000-passwords.txt', 'r')
+  
+  if use_salts:
+    slt = open('known-salts.txt', 'r')
+    for x in pwd:
+      slt.seek(0)
+      for y in slt:
+        salt = y.strip()
+        digest = hashlib.sha1(bytes(salt + x.strip() + salt, 'utf-8')).hexdigest()
+        if digest == hash:
+          return x
+          
+  else:
+    for x in pwd:
+      digest = hashlib.sha1(bytes(x, 'utf-8')).hexdigest()
+      if digest == hash:
+        return x
+        
+  return "PASSWORD NOT IN DATABASE"
